50f7ed9f1a838f961f74f3ae308e05846715aa84
Hector Martinez <hamp.martinez@gmail.com>
2018-10-26 00:00:38 -0300

    test mongo remote

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 3000;
 //var path = "mongodb://localhost:27017/mobyd";
 var path = "mongodb://MongoBD:Hampmobyd1@ds137581.mlab.com:37581/mobyd";
 
-mongoose.connect(path, { useNewUrlParser: true },(err,res)=>{
+mongoose.connect("mongodb://mobyd:a123456@ds137581.mlab.com:37581/mobyd", { useNewUrlParser: true },(err,res)=>{
     if(err){
         throw err;
     }
