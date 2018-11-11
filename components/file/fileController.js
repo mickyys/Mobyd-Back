2f3921b952d93eccd931be20295b4618b56bf496
Hector Martinez <hamp.martinez@gmail.com>
2018-11-11 04:02:48 -0300

    download google drive

--- components/file/fileController.js
+++ components/file/fileController.js
@@ -3,37 +3,54 @@
 const { File } = require('./file');
 const config = require('config');
 const fs = require('fs');
-const { Drive } = require('../google/drive/drive')
+const { Drive } = require('../google/drive/drive');
 
-async function addFile (newFile){
-    let file = new File(newFile);    
+async function addFile(newFile) {
+    let file = new File(newFile);
     file = await file.save();
 
     fs.writeFile(
-        config.get("pathFile") + file._id + "." + file.extension, 
-        newFile.fileBase64, 
-        {encoding: 'base64'}, 
+        config.get("pathFile") + file._id + "." + file.extension,
+        newFile.fileBase64, {
+            encoding: 'base64'
+        },
         (err) => {
             if (err) throw err;
             console.log('archivo almacenado correctamente');
-    });
+        });
 
     return file;
 }
 
-async function downloadFile(req , res){
-    const file = await File.findById(req.params.id);
-    const path = config.get("pathFile") + file._id + "." + file.extension;
+async function downloadFile(req, res) {
+    //const file = await File.findById(req.params.id);
+    //   const path = config.get("pathFile") + file._id + "." + file.extension;
 
-    res.download(path, file.name);
+    //  res.download(path, file.name);
+
+    const drive = new Drive();
+    console.log(0);
+    const file = await drive.downloadFile(req.params.id, 'image/jpeg');
+    console.log(4);
+    setTimeout(() => {
+        if (req.query.format === 'base64') {
+            let body = fs.readFileSync(file);
+            const fileBase64 = body.toString('base64');
+            res.send(fileBase64);
+        } else {
+            res.download(file);
+        }
+    }, 1000);
+
 }
 
-async function getFiles(req, res){
+async function getFiles(req, res) {
     const drive = new Drive();
     const files = await drive.listFiles();
 
     res.send(files);
 }
+
 
 module.exports.addFile = addFile;
 module.exports.downloadFile = downloadFile;
--- components/google/drive/drive.js
+++ components/google/drive/drive.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { google } = require('googleapis');
+const mimeType = require('mime-types');
 const tokenFile = require('./token.json');
 const credentialsFile = require('./credentials.json');
 
@@ -25,23 +26,41 @@ class Drive {
             redirect_uris
         } = credentialsFile.installed;
 
-        this.oAuth2Client =  new google.auth.OAuth2(
+        let oAuth2Client =  new google.auth.OAuth2(
             client_id, client_secret, redirect_uris[0]);
 
-        this.oAuth2Client.setCredentials(tokenFile);                    
+        oAuth2Client.setCredentials(tokenFile);      
+        
+        this.drive = google.drive({
+            version: 'v3',
+            auth : oAuth2Client
+        });
     }
 
     /**
      * Retorna lista de archivos almacenados en Google Drive
      */
     async listFiles() {
-        const drive = google.drive({
-            version: 'v3',
-            auth : this.oAuth2Client
-        });
-     
-        const listFiles = await drive.files.list();
+        const listFiles = await this.drive.files.list();
         return listFiles.data.files;
+    }
+
+    /**
+     * @param { String } id archivo Google Drive  
+     * @param { MimeType } mime para reconocer la extension del archivo
+     */
+    async downloadFile(id, mime){
+        let ext = mimeType.extension(mime);
+        let fileName = Date.now() + '.' + ext;
+        let dest = fs.createWriteStream('./tmp/' + fileName );
+
+        console.log(1);
+        const files = await this.drive.files.get({fileId: id, alt: 'media'}, {responseType: 'stream'});        
+        console.log(2);
+        const pipe = await files.data.on('end', () => { console.log('2.1'); }).pipe(dest);
+        console.log(3);
+        return pipe.path;
+        
     }
 }
 
